aa65dbc622e68a6eb7c936e3ab248424a19ff444
Bobby BuffaloBoy <novenarii@gmail.com>
2019-06-13 01:12:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,13 @@
 #write your code here
+def countdown_with_sleep(y)
+  return countdown sleep(y)
+end
 
 def countdown(x)
   while x > 0
     puts "#{x} SECOND(S)!"
     x -= 1
-
+countdown_with_sleep
 end
 return "HAPPY NEW YEAR!"
-end
-
-def countdown_with_sleep(y)
-  return countdown sleep(y)
 end
